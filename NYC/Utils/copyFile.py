ca7a793e3b4fa76bf63baacc57bc01562aed4f51
reisiiiiiiiiii <reiss_leon@web.de>
2024-03-27 19:50:46 +0000

    Insert comment here

--- NYC/Utils/copyFile.py
+++ NYC/Utils/copyFile.py
@@ -3,25 +3,25 @@ import os
 def copyFileFromTo(fileStart, target, destList):
     currentPath = os.path.dirname(os.path.abspath(__file__))
     print(currentPath)
-    path_to_files = os.path.join(currentPath, '/GraphFusionModel')  # Hier den Pfad anpassen
+    path_to_files = "/content/masterthesis/NYC/GraphFusionModel" # vollständiger Pfad zu den GraphFusionModel-Dateien
     print(path_to_files)
     allFileList = [e for e in os.listdir(path_to_files) if e.startswith(fileStart) and e.endswith('.py')]
     allFileName = [e[:-3] for e in allFileList]
 
-    rankRange = destList
     sourceRank = target
 
     sourceFile = ''
     targetFileList = []
-    for i in range(len(allFileName)):
-        fileName = allFileName[i].split('_')
-        rank = int(fileName[-1])
-        if rank == sourceRank:
-            sourceFile = allFileList[i]
-        elif rank >= min(rankRange) and rank <= max(rankRange):
-            targetFileList.append(allFileList[i])
+    for fileName in allFileName:
+        rank_str = fileName.split('V')[-1] # Extrahiere den Rang als Zeichenfolge nach 'V'
+        if rank_str.isdigit(): # Überprüfe, ob der Rang eine Zahl ist
+            rank = int(rank_str)
+            if rank == sourceRank:
+                sourceFile = fileName + '.py' # Füge die Dateierweiterung '.py' hinzu
+            elif rank >= destList[0] and rank <= destList[1]:
+                targetFileList.append(fileName + '.py')
 
-    # get source file content
+    # Quelldateiinhalt erhalten
     if sourceFile == '':
         print('Can not find source file')
     else:
@@ -31,14 +31,16 @@ def copyFileFromTo(fileStart, target, destList):
             print('No target file')
         else:
             for targetFile in targetFileList:
-                with open(os.path.join(currentPath, targetFile), 'w', encoding='utf-8') as f:
+                with open(os.path.join(currentPath, targetFile + '.py'), 'w', encoding='utf-8') as f:
                     f.writelines(sourceFileContent)
     print('Succeed')
-    print("Dateien im Verzeichnis:", os.listdir(currentPath))
+    print("Dateien im Verzeichnis:", os.listdir(path_to_files))
 
 
 if __name__ == '__main__':
-    # copyFileFromTo('GraphSingleStationDemandPre', 0, [1, 9])
-    copyFileFromTo('GraphSingleStationDemandPreV2', 0, [1, 299])
-    copyFileFromTo('GraphFusionModel_', 0, [1, 299])
-    # generateFile('GraphFusionModel_%s.py', 0, [1, 299])
+    copyFileFromTo('GraphFusionModelV', 14, [1, 299])
+
+    #copyFileFromTo('GraphSingleStationDemandPre', 0, [1, 9])
+    #copyFileFromTo('GraphSingleStationDemandPreV2', 0, [1, 299])
+    #copyFileFromTo('GraphFusionModel_', 0, [1, 299])
+    #generateFile('GraphFusionModel_%s.py', 0, [1, 299])
